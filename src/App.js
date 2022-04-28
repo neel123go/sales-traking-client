0d71c27abad474a3fa968b74a3f5f1bb1a76e7dd
neel123go <ayounpaul1029@gmail.com>
2022-04-28 22:26:18 +0600

    header design completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home';
+import Header from './Components/Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>This is app component</h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Pages/Shared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/home">WebSite Name</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
